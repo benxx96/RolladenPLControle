25831b6c70ac4a296b196a9a4332b96271eed511
benxx96 <140310837+benxx96@users.noreply.github.com>
2023-11-17 12:58:49 +0100

    serial communication

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,70 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <SoftwareSerial.h>
+
+// Define
+#define RX_PIN 10   // Virtual Reciever Pin
+#define TX_PIN 11   // Virtual Transmitter Pin
+
+// Variable
+SoftwareSerial mySerial(RX_PIN, TX_PIN); // RX, TX
+
+struct Data 
+{
+  unsigned int id = -1;
+  String content = ""; 
+};
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  mySerial.begin(9600);
+}
+
+void serialWrite(){
+  // noInterrupts();
+  Data data;
+  data.id = 1;
+  data.content = "test";
+
+  if (mySerial.available()){
+    // begin-marker
+    mySerial.print("**");
+    
+    // content
+    mySerial.print("id:");
+    mySerial.print(data.id);
+    mySerial.print(";");
+
+    mySerial.print("content:");
+    mySerial.print(data.content);
+
+    // end-marker
+    mySerial.print("$$");
+  }
+}
+
+void serialRead(){
+  String raw_data_str = "";
+  String data_str = "";
+
+  mySerial.listen();
+  // while there is data coming in, read it
+  // and send to the hardware serial port:
+  if(mySerial.available() > 0){
+    Serial.println("Data from mySerial:");
+    while (mySerial.available() > 0) {
+      char inByte = mySerial.read();
+      // Serial.write(inByte);
+      raw_data_str += inByte;
+    }
+    data_str = raw_data_str.substring(raw_data_str.indexOf('*')+2, raw_data_str.indexOf('$'));
+    Serial.println(data_str);
+    Serial.println();
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // serialWrite();
+  serialRead();
+  delay(800);
 }
