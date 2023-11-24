969271f6da4b7709fe76698aeff257c301b2574f
benxx96 <140310837+benxx96@users.noreply.github.com>
2023-11-24 16:03:55 +0100

    byte comunication working

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,16 @@
 
 
 // Variable
+int bufferSize = 0;
+byte status = 0;
 bool sender = false;
+bool sendData = false;
 bool isSendingData = false;
-bool hasSendedData = false;
 bool hasReceivedData = false;
+unsigned long receiveTimer = 0;
+unsigned long receiveTimerLength = 200;
 unsigned long sendTimer = 0;
-unsigned long sendTimerLength = 2000;
+unsigned long sendTimerLength = 5000;
 
 
 // Objects
@@ -49,7 +53,16 @@ String serialRead(){
   // while there is data coming in, read it
   // and send to the hardware serial port:
   char inByte = (char)mySerial.read();
-  if(inByte == '*' && mySerial.available() > 6){
+  if(mySerial.available() > 1){Serial.print(inByte);}
+  // Serial.print(inByte);
+  if(inByte == '*'){
+    int i=0;
+    while(mySerial.available() < 7){
+      delay(100);
+      Serial.print(".");
+      if(i>500){break;}
+      i++;
+    }
     for(int i=0; i<6; i++){
       inByte = (char)mySerial.read();
       if((int)inByte >= 0 && (int)inByte <= 127){
@@ -58,52 +71,76 @@ String serialRead(){
       else{
         i--;
       }
-      if(inByte == '$'){break;}
+      if(inByte == '$'){
+        bufferSize = mySerial.available();
+        while (mySerial.available() > 1)
+        {
+          mySerial.read();
+        }
+        Serial.println();
+        Serial.print(bufferSize);
+        Serial.print(" - reset buffer to: ");
+        Serial.println(mySerial.available());
+        break;
+      }
     }
   }
-  delay(100);
+  delay(50);
   return data;
 }
 
 void loop() {
   if(sender){
-    if(digitalRead(8) == HIGH){
+    if(digitalRead(BUTTON_PIN) == HIGH){
+      sendData = true;
       isSendingData = true;
-      hasSendedData = false;
       hasReceivedData = false;
     }
 
-    if(millis() > sendTimerLength + sendTimer){
-      if(isSendingData && !hasSendedData && !hasReceivedData){
-        digitalWrite(LED_PIN, HIGH);
-        serialWrite('0', '1', '1', '0');
-        isSendingData = false;
-        hasSendedData = true;
-        Serial.println("send");
-      }
-      else if(!isSendingData && hasSendedData && !hasReceivedData){
+    if(sendData && millis() > sendTimerLength + sendTimer){
+      isSendingData = true;
+      sendTimer = millis();
+      // Serial.println("resend activate");
+    }
+
+    if(millis() > receiveTimerLength + receiveTimer){
+      if(!isSendingData){
         String data;
         char code[] = "*0111$";
         data = serialRead();
-        if (strcmp(data.c_str(), code) == 0){
-          hasSendedData = false;
+        if (strcmp(data.c_str(), code) == 0 && !hasReceivedData){
+          sendData = false;
+          isSendingData = false;
           hasReceivedData = true;
           digitalWrite(LED_PIN, LOW);
+
+          Serial.println(data);
         }
-        else{
-          isSendingData = true;
-          hasSendedData = false;
-          hasReceivedData = false;
-        }
+        // Serial.println("receiving");
       }
-      sendTimer = millis();
+      else if(sendData && isSendingData && !hasReceivedData){
+        digitalWrite(LED_PIN, HIGH);
+        serialWrite('0', '1', '1', '0');
+        isSendingData = false;
+        Serial.println("sending");
+
+        bufferSize = mySerial.available();
+        while (mySerial.available() > 1)
+        {
+          mySerial.read();
+        }
+        Serial.println();
+        Serial.print(bufferSize);
+        Serial.print(" - reset buffer to: ");
+        Serial.println(mySerial.available());
+      }
+      receiveTimer = millis();
     } 
   }
   else{
-    if(millis() > sendTimerLength + sendTimer){
+    if(millis() > receiveTimerLength + receiveTimer){
       String data;
-
-      if(!hasReceivedData && !isSendingData){
+      if(!isSendingData){
         char code[] = "*0110$";
         data = serialRead();
         if (strcmp(data.c_str(), code) == 0){
@@ -113,12 +150,32 @@ void loop() {
           Serial.println(data);
         }
       }
-      else if(hasReceivedData && isSendingData){
-        serialWrite('0', '1', '1', '1');
-        hasReceivedData = false;
-        isSendingData = false;
+      else if(isSendingData){
+        for(int i=0; i<20; i++){
+          serialWrite('0', '1', '1', '1');
+          isSendingData = false;
+          Serial.println("sending");
+        }
+        if(status == HIGH){
+          digitalWrite(LED_PIN, LOW);
+          status = LOW;
+        }
+        else{
+          digitalWrite(LED_PIN, HIGH);
+          status = HIGH;
+        }
+
+        bufferSize = mySerial.available();
+        while (mySerial.available() > 1)
+        {
+          mySerial.read();
+        }
+        Serial.println();
+        Serial.print(bufferSize);
+        Serial.print(" - reset buffer to: ");
+        Serial.println(mySerial.available());
       }
-      sendTimer = millis();
+      receiveTimer = millis();
     } 
   }
   delay(50);
